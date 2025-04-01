d8e1824a66b23256e65a292771e2f75a55a4506b
Timm Bremus <timm.bremus@knx-helden.de>
2025-04-01 17:25:44 +0200

    Tab Navigation eingebaut

--- KnxHelden.SHES.App/Views/MasterDataView.xaml.cs
+++ KnxHelden.SHES.App/Views/MasterDataView.xaml.cs
@@ -1,6 +1,7 @@
 using KnxHelden.SHES.App.ViewModels;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Media.Animation;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -12,12 +13,41 @@ namespace KnxHelden.SHES.App.Views
     /// </summary>
     public sealed partial class MasterDataView : Page
     {
+        private int previousSelectedIndex = 0;
+
         public MasterDataViewModel ViewModel { get; }
 
         public MasterDataView()
         {
             ViewModel = App.Services.GetService<MasterDataViewModel>();
             this.InitializeComponent();
+        }
+
+        private void MasterDataNavigation_SelectionChanged(SelectorBar sender, SelectorBarSelectionChangedEventArgs args)
+        {
+            SelectorBarItem selectedItem = sender.SelectedItem;
+            int currentSelectedIndex = sender.Items.IndexOf(selectedItem);
+            System.Type pageType;
+
+            switch (currentSelectedIndex)
+            {
+                case 0:
+                    pageType = typeof(CompanyView);
+                    break;
+                case 1:
+                    pageType = typeof(ManufacturerView);
+                    break;
+                default:
+                    pageType = typeof(CompanyView);
+                    break;
+            }
+
+            var slideNavigationTransitionEffect = currentSelectedIndex - previousSelectedIndex > 0 ? SlideNavigationTransitionEffect.FromRight : SlideNavigationTransitionEffect.FromLeft;
+
+            ContentFrame.Navigate(pageType, null, new SlideNavigationTransitionInfo() { Effect = slideNavigationTransitionEffect });
+
+            previousSelectedIndex = currentSelectedIndex;
+
         }
     }
 }
