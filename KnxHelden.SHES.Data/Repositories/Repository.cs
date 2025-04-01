b08bd5ed3781aa07bb1a4363bc324410d5cfdf88
Timm Bremus <timm.bremus@knx-helden.de>
2025-04-01 18:15:40 +0200

    Bugfixes an der Speicherlogik

--- KnxHelden.SHES.Data/Repositories/Repository.cs
+++ KnxHelden.SHES.Data/Repositories/Repository.cs
@@ -149,6 +149,10 @@ namespace KnxHelden.SHES.Data.Repositories
 
             try
             {
+                // Set creation date and last modification time
+                entity.CreationTime = DateTime.Now;
+                entity.LastModificationTime = DateTime.Now;
+
                 await this._dbContext.AddAsync(entity);
                 await this._dbContext.SaveChangesAsync();
                 this._logger.LogInformation($"A new entity (Id: {entity.Id}) was added.");
@@ -173,6 +177,9 @@ namespace KnxHelden.SHES.Data.Repositories
             try
             {
                 this.DetachEntity(entity);
+
+                // Set last modification time
+                entity.LastModificationTime = DateTime.Now;
 
                 // set Modified flag in your entry
                 this._dbContext.Entry(entity).State = EntityState.Modified;
