45f2a6dba3c22291f7f5160029ce0a3ba8197495
Timm Bremus <timm.bremus@knx-helden.de>
2025-04-03 14:42:55 +0200

    Dokumentation

--- KnxHelden.SHES.Services/Projects/IProjectService.cs
+++ KnxHelden.SHES.Services/Projects/IProjectService.cs
@@ -6,10 +6,40 @@ namespace KnxHelden.SHES.Services.Projects
 {
     public interface IProjectService : IService
     {
-        Task<bool> DeleteAsync(ObservableProject observableProject);
+        /// <summary>
+        /// Checks if a project with the specified name exists.
+        /// </summary>
+        /// <param name="name">The name of the project.</param>
+        /// <returns>True if the project exists; otherwise, false.</returns>
         Task<bool> ExistsAsync(string name);
+
+        /// <summary>
+        /// Retrieves all projects.
+        /// </summary>
+        /// <returns>A collection of all projects.</returns>
         Task<ObservableCollection<ObservableProject>> GetAllAsync();
-        Task<ObservableProject> InsertAsync(ObservableProject observableProject);
+
+        /// <summary>
+        /// Adds a new project.
+        /// </summary>
+        /// <param name="observableProject">The project to be added.</param>
+        /// <returns>The newly created project with an auto-generated ID.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when the project name is null or empty.</exception>
+        /// <exception cref="EntityAlreadyExistsException">Thrown when a project with the same name already exists.</exception>
+        Task<ObservableProject> AddAsync(ObservableProject observableProject);
+
+        /// <summary>
+        /// Updates an existing project.
+        /// </summary>
+        /// <param name="observableProject">The project to be updated.</param>
+        /// <returns>The updated project if successful; otherwise, null.</returns>
         Task<ObservableProject> UpdateAsync(ObservableProject observableProject);
+
+        /// <summary>
+        /// Deletes an existing project.
+        /// </summary>
+        /// <param name="observableProject">The project to be deleted.</param>
+        /// <returns>True if the deletion was successful; otherwise, false.</returns>
+        Task<bool> DeleteAsync(ObservableProject observableProject);
     }
 }
--- KnxHelden.SHES.Services/Projects/ProjectService.cs
+++ KnxHelden.SHES.Services/Projects/ProjectService.cs
@@ -13,20 +13,33 @@ using System.Threading.Tasks;
 
 namespace KnxHelden.SHES.Services.Projects
 {
+    /// <summary>
+    /// Service class for managing projects.
+    /// </summary>
     public class ProjectService : ServiceBase, IProjectService
     {
         private readonly IProjectRepository _projectRepository;
 
+        #region --- Constructors ---
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ProjectService"/> class.
+        /// </summary>
+        /// <param name="resourceLoader">The resource loader for localization.</param>
+        /// <param name="logger">The logger for logging messages.</param>
+        /// <param name="projectRepository">The project repository for data access.</param>
         public ProjectService(ResourceLoader resourceLoader, ILogger<ProjectService> logger, IProjectRepository projectRepository)
             : base(resourceLoader, logger)
         {
             this._projectRepository = projectRepository;
         }
 
+        #endregion
+
         /// <summary>
-        /// Gets all projects.
+        /// Retrieves all projects.
         /// </summary>
-        /// <returns>Returns a list of all projects.</returns>
+        /// <returns>A collection of all projects.</returns>
         public async Task<ObservableCollection<ObservableProject>> GetAllAsync()
         {
             try
@@ -41,14 +54,13 @@ namespace KnxHelden.SHES.Services.Projects
         }
 
         /// <summary>
-        /// Inserts a new project.
+        /// Adds a new project.
         /// </summary>
-        /// <param name="name">The project name.</param>
-        /// <param name="number">The unique project number.</param>
-        /// <param name="customer">The customer name.</param>
-        /// <param name="address">The project address.</param>
-        /// <returns>Returns the new project with auto-generated Id.</returns>
-        public async Task<ObservableProject> InsertAsync(ObservableProject observableProject)
+        /// <param name="observableProject">The project to be added.</param>
+        /// <returns>The newly created project with an auto-generated ID.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when the project name is null or empty.</exception>
+        /// <exception cref="EntityAlreadyExistsException">Thrown when a project with the same name already exists.</exception>
+        public async Task<ObservableProject> AddAsync(ObservableProject observableProject)
         {
             if (string.IsNullOrWhiteSpace(observableProject.Name))
             {
@@ -85,6 +97,11 @@ namespace KnxHelden.SHES.Services.Projects
             }
         }
 
+        /// <summary>
+        /// Updates an existing project.
+        /// </summary>
+        /// <param name="observableProject">The project to be updated.</param>
+        /// <returns>The updated project if successful; otherwise, null.</returns>
         public async Task<ObservableProject> UpdateAsync(ObservableProject observableProject)
         {
             try
@@ -99,6 +116,11 @@ namespace KnxHelden.SHES.Services.Projects
             }
         }
 
+        /// <summary>
+        /// Deletes an existing project.
+        /// </summary>
+        /// <param name="observableProject">The project to be deleted.</param>
+        /// <returns>True if the deletion was successful; otherwise, false.</returns>
         public async Task<bool> DeleteAsync(ObservableProject observableProject)
         {
             try
@@ -113,6 +135,11 @@ namespace KnxHelden.SHES.Services.Projects
             return true;
         }
 
+        /// <summary>
+        /// Checks if a project with the specified name exists.
+        /// </summary>
+        /// <param name="name">The name of the project.</param>
+        /// <returns>True if the project exists; otherwise, false.</returns>
         public async Task<bool> ExistsAsync(string name)
         {
             return await this._projectRepository.ExistsAsync(name);
