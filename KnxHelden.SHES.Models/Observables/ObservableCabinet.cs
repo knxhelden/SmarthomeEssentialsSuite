587b9cf677a13111c7a6c4f7ddad7ff56cee0d9a
Timm Bremus <timm.bremus@knx-helden.de>
2025-06-04 16:51:44 +0200

    Neue Observable für einen Schaltschrank

---
+++ KnxHelden.SHES.Models/Observables/ObservableCabinet.cs
@@ -0,0 +1,32 @@
+﻿using KnxHelden.SHES.Models.Entities;
+
+namespace KnxHelden.SHES.Models.Observables
+{
+    public class ObservableCabinet : ObservableBase<Cabinet>
+    {
+        private readonly ObservableProjectItem _projectItem;
+
+        #region --- Properties ---
+
+        public ObservableProjectItem ProjectItem
+        {
+            get => _projectItem;
+        }
+
+        #endregion
+
+        #region --- Constructors ---
+
+        public ObservableCabinet()
+            : this(new Cabinet())
+        { }
+
+        public ObservableCabinet(Cabinet cabinet)
+            : base(cabinet)
+        {
+            _projectItem = new ObservableProjectItem(cabinet);
+        }
+
+        #endregion
+    }
+}
